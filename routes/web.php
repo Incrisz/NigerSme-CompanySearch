bccf3f4c3ae216c5975c0626b09793499dae8283
incrisz <incrisz4luv@gmail.com>
2025-02-07 14:36:43 +0100

    dd

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@ use App\Http\Controllers\CompanyController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/companies/search', [CompanyController::class, 'search'])->name('companies.search');
 
-Route::resource('companies', CompanyController::class);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('companies', CompanyController::class);
